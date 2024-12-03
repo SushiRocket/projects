d1394f8772ce75352c319bc421b44cb715cf5f45
SushiRocket <kentamori27@gmail.com>
2024-12-03 22:11:46 +0900

    change tweet_form.html to tweet_create.html

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from.views import IndexView
+from.views import IndexView,TweetCreateView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('create/', TweetCreateView.as_view(), name='tweet_create'),
 ]
