91fc4e70b557ee1af42475435a5ef107cb667582
SushiRocket <kentamori27@gmail.com>
2024-12-04 10:07:41 +0900

    add SignUpForm at forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from.models import Tweet
 
 class TweetForm(forms.ModelForm):
-    content = forms.CharField(
+    content=forms.CharField(
         widget=forms.Textarea,
         max_length='1000',
         required=True,
@@ -12,3 +12,32 @@ class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['content']
+
+class SignUpForm(forms.ModelForm):
+    password1=forms.CharField(
+        label='パスワード',
+        widget=forms.PasswordInput,
+    )
+
+    password2=forms.CharField(
+        label='再確認用パスワード',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = 'User'
+        fields = ('username', 'password1', 'password2')
+
+    def clean_password2(self):
+        password1=self.cleaned_data.get('password1')
+        password2=self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('パスワードが一致しません！')
+        return password2
+
+    def save(self, commit=True):
+        user=super(SignUpForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+            return user
