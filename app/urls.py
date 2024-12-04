61de06b32ef851905baf2a43da7f8804161a4f3f
SushiRocket <kentamori27@gmail.com>
2024-12-04 09:06:28 +0900

    update urls.py

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('create/', TweetCreateView.as_view(), name='tweet_create'),
 
     #認証関連
-    path('login/', auth_views,LoginView.as_view(template_name='app/login.html'), name='login'),
-    path('logout/', auth_views,LogoutView.as_view(template_name='app/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='app/logout.html'), name='logout'),
 ]
