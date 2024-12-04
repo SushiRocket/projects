b801df1c000dd4db7020c17b45ea893c8db4492b
SushiRocket <kentamori27@gmail.com>
2024-12-04 17:14:42 +0900

    change Path TweetDetailView at urls.py

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('create/', TweetCreateView.as_view(), name='tweet_create'),
     path('signup', SignUpView.as_view(), name='signup'),
-    path('detail<int:post_pk>/', TweetDetailView.as_view(), name='tweet_detail'),
+    path('tweet_detail/<int:pk>/', TweetDetailView.as_view(), name='tweet_detail'),
 
     #認証関連
     path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
