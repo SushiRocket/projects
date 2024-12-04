5bd929cdc5d6d5e7c5a9e8e690f1a2263f1f04fd
SushiRocket <kentamori27@gmail.com>
2024-12-04 17:07:01 +0900

    add Path TweetDetailView at urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from.views import IndexView,TweetCreateView,SignUpView
+from.views import IndexView,TweetCreateView,SignUpView,TweetDetailView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('create/', TweetCreateView.as_view(), name='tweet_create'),
     path('signup', SignUpView.as_view(), name='signup'),
+    path('detail<int:post_pk>/', TweetDetailView.as_view(), name='tweet_detail'),
 
     #認証関連
     path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
