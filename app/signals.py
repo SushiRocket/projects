0d6914b2237d563817a2aff627de60824106bf96
SushiRocket <kentamori27@gmail.com>
2024-12-06 19:37:49 +0900

    Create def create_profile at  signals.py

---
+++ app/signals.py
@@ -0,0 +1,10 @@
+#djangoの機能。特定のイベント発生時に発生する特定の処理を実行する仕組み
+from django.db.models.signals import post_save
+from django.dispatch import receiver #シグナルの処理を紐づけるためのデコレーター
+from django.contrib.auth.models import User
+from.models import Profile
+
+@receiver(post_save, sender=User)#Userモデルにたいして動作
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.object.create(user=instance)
