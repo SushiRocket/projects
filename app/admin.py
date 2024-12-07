8fdc6b4e882a2e73cbb772be943723f122b55a29
SushiRocket <kentamori27@gmail.com>
2024-12-07 13:39:16 +0900

    Create superuser and admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Tweet, Like, User
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'avatar')
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Tweet)
+admin.site.register(Like)
+admin.site.register(User)
