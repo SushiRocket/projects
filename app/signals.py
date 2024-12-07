36c5ac97a2c04c17c3631ccfbc29ed864aba8f3c
SushiRocket <kentamori27@gmail.com>
2024-12-07 13:32:58 +0900

    fix object to objects

--- app/signals.py
+++ app/signals.py
@@ -7,7 +7,7 @@ from.models import Profile
 @receiver(post_save, sender=User)#Userモデルにたいして動作
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.object.create(user=instance)
+        Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
